00f65692b1b3da88c4dfc11a50f95c190f1b0d2d
anuragchavhan0 <anuragchavhan0@localhost>
2025-12-17 12:40:30 +0530

    added a new file

---
+++ PCE/src/com/demo/Mobapp.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+
+ class  Robot {
+	String brand;
+	String  Os;
+	int price ;
+	
+	public  void displayInfo() {
+		System.out.println("Robot details");
+		System.out.println("brand : "+ brand);
+		System.out.println("Os : "+ Os);
+		System.out.println("Price : "+ price);
+
+
+	}
+	
+}
+ 
+
+public class Mobapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  Robot R1=  new  Robot();
+		  R1.price=1223;
+		  R1.brand="hammer";
+		  R1.Os="Linux";
+		  
+		 R1.displayInfo();		  
+
+	}
+
+}
