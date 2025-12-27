a93866279eca1e05b33db4ff3745a1c7262c8ed0
anuragchavhan0 <anuragchavhan0@localhost>
2025-12-27 15:38:37 +0530

    Signiture

---
+++ PCE/src/com/staticc/Signitures.java
@@ -0,0 +1,31 @@
+package com.staticc;
+
+public class Signitures {
+
+	 static public void main(String ... args) {
+		// TODO Auto-generated method stub
+		System.out.println("PCE");
+		int a;
+		main('a');
+		main(344);
+	}
+	 static public void main(int ... args) {
+			// TODO Auto-generated method stub
+			System.out.println("main accepting integer type of array");
+			
+		}
+	 static public void main() {
+			// TODO Auto-generated method stub
+			System.out.println("main is not accepting any argument");
+			
+		}
+	 static public void main(char a) {
+			// TODO Auto-generated method stub
+			System.out.println("main accepting char type of input");
+			
+		}
+	 
+	   	
+	 
+	 
+}
