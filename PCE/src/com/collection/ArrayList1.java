0ad30a9c20d3c472bfc27d56dd7c443cf4071edd
anuragchavhan0 <anuragchavhan0@localhost>
2026-01-27 15:37:18 +0530

    collection Framework

---
+++ PCE/src/com/collection/ArrayList1.java
@@ -0,0 +1,22 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add("anurag");
+		al.add(true);
+		al.add(21.8);
+		al.add('A');
+		
+		System.out.println(al);
+		
+
+	}
+
+}
---
+++ PCE/src/com/collection/LinkedList1.java
@@ -0,0 +1,19 @@
+package com.collection;
+
+import java.util.LinkedList;
+
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList al = new LinkedList();
+		al.add(10);
+		al.add("anurag");
+		al.add(true);
+		al.add(21.8);
+		al.add('A');
+		
+		System.out.println(al);
+	}
+
+}
