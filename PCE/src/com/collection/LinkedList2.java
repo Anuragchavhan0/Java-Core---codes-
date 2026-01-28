f07304c633e400e55173de0eea845d8c2c088db5
anuragchavhan0 <anuragchavhan0@localhost>
2026-01-28 12:38:35 +0530

    front end , rare rend & in- between of the list .

---
+++ PCE/src/com/collection/LinkedList2.java
@@ -0,0 +1,19 @@
+package com.collection;
+
+import java.util.LinkedList;
+
+public class LinkedList2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList ll= new LinkedList();
+		ll.add(10);
+		ll.add(50);
+		ll.addLast(89);
+		ll.addFirst(77);
+		ll.add(45);
+		ll.add(1,909);
+		System.out.println(ll);
+	}
+
+}
