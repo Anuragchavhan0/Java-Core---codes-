2d440ac314defe54ab1a6624e3236f68da99c96a
anuragchavhan0 <anuragchavhan0@localhost>
2026-02-04 15:40:10 +0530

    .

---
+++ PCE/src/com/legacy_classes_And_sorting/CompareTo0.java
@@ -0,0 +1,14 @@
+package com.legacy_classes_And_sorting;
+
+public class CompareTo0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1 = "SACHIN";
+		String s2 = "SAVRAV";
+		int res = s1.compareTo(s2);
+		System.out.println(res);
+
+}
+}
